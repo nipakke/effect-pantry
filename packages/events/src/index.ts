ec481c39326463db47efb78f16645da65ac6e42c
nipakke <nipakke@gmail.com>
2026-05-19 00:53:13 +0200

    add events package

---
+++ packages/events/src/index.ts
@@ -0,0 +1,5 @@
+export * as Event from './Event.js';
+export * as EventBus from './EventBus.js';
+
+export { EventValidationError, EventPublishError } from './errors.js';
+export type { EventBusError } from './errors.js';
